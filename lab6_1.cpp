78110f929cbde984634b44ac1282f2477643f888
Thanaphat <thanapat30769@gmail.com>
2024-11-30 13:01:17 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int N = 1;
+    int E = 0;
+    int O = 0;
+    while(N > 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    if(N%2 == 0 && N > 0){
+        E += 1;
+    }else if(N%2 == 1){
+        O += 1;
+    }
+}
+    cout << "#Even numbers = " << E << endl;
+    cout << "#Odd numbers = " << O;
     return 0;
 }
